a4f424b97d223075dc6c717bfdd0f5c09e50bb29
Ruan Araújo <ruands2002@gmail.com>
2022-08-04 21:00:04 -0300

    feat: add parametros do jogo

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import params from "./src/params";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Iniciando o Mines!</Text>
+      <Text>
+        Tamanho da grade:
+        {params.getRowsAmount()}x{params.getColumnsAmount()}
+      </Text>
     </View>
   );
 }
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
